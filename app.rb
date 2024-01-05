4b99591f9516c91c4e5beb09cef5186898b2bf7e
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-01-05 04:50:06 +0000

    created homepage and developed another route to send api call to get pokemon data.

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:index)
+end
+
+get("/generate_pokemon_team") do
+  @random_poke_indexes=1.times.map{rand(1..1025).to_s} #adjust the number to 6 later. for now develop app for one pokemon.
+  poke_api_url= "https://pokeapi.co/api/v2/pokemon/"
+  @pokemon_team_data=[]
+  
+  @random_poke_indexes.each do |poke_index|
+    response= HTTP.get(poke_api_url+poke_index)
+    @pokemon_team_data << JSON.parse(response)
+  end
+  
+
+  erb(:generate_pokemon_form)
 end
