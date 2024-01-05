db93c9a142bb170a20b88ca7cb4a73bf94efa898
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-01-05 22:17:14 +0000

    implemented displaying pokemon through card format including name and image using css.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ get("/") do
 end
 
 get("/generate_pokemon_team") do
-  @random_poke_indexes=1.times.map{rand(1..1025).to_s} #adjust the number to 6 later. for now develop app for one pokemon.
+  @random_poke_indexes=6.times.map{rand(1..1025).to_s}
   poke_api_url= "https://pokeapi.co/api/v2/pokemon/"
   @pokemon_team_data=[]
   
